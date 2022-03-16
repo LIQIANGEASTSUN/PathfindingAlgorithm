f51fc2dd4f9181b33d7549fc814b8209fe82a203
LQ <liqiangeastsun@163.com>
2022-03-16 20:54:08 +0800

    no message

---
+++ Labyrinth2/Labyrinth/Assets/Sprites/TextureChange/LabyrinthTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthTexture : MonoBehaviour
+{
+    public static LabyrinthTexture labyrinthTexture;
+    private List<Texture2D> textureList = new List<Texture2D>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        labyrinthTexture = this;
+
+        TextureChange textureChange = new TextureChange();
+        for (int i = 0; i < 15; ++i)
+        {
+            //string filePath = string.Format("Assets/Resources/Tile{0}.png", i);
+            string filePath = string.Format("Tile{0}.png", i);
+            Texture2D texture2D = Resources.Load<Texture2D>(filePath);
+            textureChange.WriteColor(i, texture2D);
+            textureList.Add(texture2D);
+        }
+    }
+
+    public List<Texture2D> TextureList
+    {
+        get { return textureList; }
+    }
+    
+}
---
+++ Labyrinth2/Labyrinth/Assets/Sprites/TextureChange/TextureChange.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChange
+{
+    private int borderWidth = 6;
+
+    public void WriteColor(int index, Texture2D texture2D)
+    {
+        WriteUp(index, texture2D);
+        WriteDown(index, texture2D);
+        WriteLeft(index, texture2D);
+        WriteRight(index, texture2D);
+        texture2D.Apply();
+    }
+
+    private void WriteUp(int index, Texture2D texture2D)
+    {
+        if ((index & 1) == 0)
+        {
+            return;
+        }
+
+        Write(0, texture2D.width, texture2D.height - borderWidth, texture2D.height, texture2D);
+    }
+
+    private void WriteDown(int index, Texture2D texture2D)
+    {
+        if ((index & 2) == 0)
+        {
+            return;
+        }
+        Write(0, texture2D.width, 0, borderWidth, texture2D);
+    }
+
+    private void WriteLeft(int index, Texture2D texture2D)
+    {
+        if ((index & 4) == 0)
+        {
+            return;
+        }
+        Write(0, borderWidth, 0, texture2D.height, texture2D);
+    }
+
+    private void WriteRight(int index, Texture2D texture2D)
+    {
+        if ((index & 8) == 0)
+        {
+            return;
+        }
+        Write(texture2D.width - borderWidth, texture2D.width, 0, texture2D.height, texture2D);
+    }
+
+    private void Write(int startX, int endX, int startY, int endY, Texture2D texture2D)
+    {
+        for (int i = startX; i < endX; ++i)
+        {
+            for (int j = startY; j < endY; ++j)
+            {
+                texture2D.SetPixel(i, j, Color.white);
+            }
+        }
+    }
+}
