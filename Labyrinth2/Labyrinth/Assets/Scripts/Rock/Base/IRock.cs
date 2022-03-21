559f54912c6ca5c545887efb76689b34bcb696f8
LQ <liqiangeastsun@163.com>
2022-03-21 11:32:14 +0800

    no message

--- Labyrinth2/Labyrinth/Assets/Scripts/Rock/Base/IRock.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface IRock
-{
-    void Begin(Vector2 pos);
-    void Move(Vector2 pos, float percentage);
-    void End(Vector2 pos);
-}
--- Labyrinth2/Labyrinth/Assets/Scripts/Tools/Utils/GameConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameConstants
-{
-    //预设分辨率
-    public static Vector2 GAME_RESOLUTION = new Vector2(1136f, 640f);
-}
